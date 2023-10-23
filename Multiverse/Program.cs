0a0a2b57f2288af0fa9b0ca5ecd6ccee0ad573bb
PrSomni <gabrield@hotmail.fr>
2023-10-23 11:06:31 +0200

    Modif HelloWorld to WoldA

--- Multiverse/Program.cs
+++ Multiverse/Program.cs
@@ -10,7 +10,7 @@ namespace Multiverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Master!");
+            Console.WriteLine("Hello World A!");
         }
     }
 }
